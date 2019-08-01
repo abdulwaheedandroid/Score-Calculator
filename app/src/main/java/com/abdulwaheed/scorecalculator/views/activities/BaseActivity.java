73921cf40552d12fc16741cc6ed6aa606a018b8a
Abdul Waheed <bawan@abacusoft.com>
2019-08-01 23:37:56 +0500

    Splash Work done

--- app/src/main/java/com/abdulwaheed/scorecalculator/views/activities/BaseActivity.java
+++ app/src/main/java/com/abdulwaheed/scorecalculator/views/activities/BaseActivity.java
@@ -1,6 +1,19 @@
 package com.abdulwaheed.scorecalculator.views.activities;
 
+import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+
+    protected void navigate(Class<?> activity) {
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+    }
+
+    protected void navigate(Class<?> activity, final int flag) {
+        Intent intent = new Intent(this, activity);
+        intent.addFlags(flag);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/abdulwaheed/scorecalculator/views/activities/SplashActivity.java
+++ app/src/main/java/com/abdulwaheed/scorecalculator/views/activities/SplashActivity.java
@@ -1,4 +1,21 @@
 package com.abdulwaheed.scorecalculator.views.activities;
 
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+import com.abdulwaheed.scorecalculator.views.R;
+
 public class SplashActivity extends BaseActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2_000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> navigate(MainActivity.class), SPLASH_DISPLAY_LENGTH);
+    }
 }
